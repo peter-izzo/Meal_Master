1fd3d9a8163cf5124fc639b556557e3530327bc7
Peter Izzo <peter.izzo528@gmail.com>
2019-07-18 06:23:41 -0400

    Meal Master recipe look up app first commit. basic framework code

---
+++ src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+      const proxy = 'https://cors-anywhere.herokuapp.com/';
+      const key = 'ec85142a98cef7a8e40d17a5ac013536';
+      try{
+          const res =  await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+          this.result = res.data.recipes;
+          //console.log(this.result);
+      } catch (error) {
+          alert(error);
+      }
+    }
+}
