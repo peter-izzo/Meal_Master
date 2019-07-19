2618ba8c9ede86ccbb71b8fb2116f017df14eb99
Peter Izzo <peter.izzo528@gmail.com>
2019-07-19 06:50:32 -0400

    added config file for 'safer' api calling, pagnation btns & logic asso.

---
+++ src/js/models/Recipe.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { key, proxy } from '../config'
+
+
+export default class Recipe {
+  constructor(id) {
+    this.id = id;
+  }
+
+  async getRecipe() {
+    try {
+        const res = await axios(`${proxy}https://www.food2fork.com/api/get?key=${key}&rId=${this.id}`);
+        console.log(res);
+    } catch (error) {
+        console.log(error);
+    }
+  }
+}
